420f7cbf1f2fed1fd318fffbf31d44fc1351f4a7
Ihor Kucher <IhorKucher25@kse.org.ua>
2026-03-08 23:53:53 +0200

    Added basic class with interface

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace ConsoleApp8;
+﻿
+namespace ConsoleApp8;
 
 public interface IUniversityMember
 {
@@ -8,3 +9,42 @@ public interface IUniversityMember
     void PerformDuties();
 }
 
+public class UniversityMember : IUniversityMember
+{
+    private string _name;
+    public string Name 
+    {
+        get
+        {
+            return _name;
+            
+        }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+
+            _name = value;
+        }
+    }
+    public string MemberId { get; }
+    public List<string> ActionLog { get; set; } = new List<string>();
+
+    public virtual void PerformDuties()
+    {
+       
+        if (ActionLog.Count >= 5)
+        {
+            throw new Exception("Daily limit of 5 actions reached.");
+        }
+    }
+
+    public UniversityMember(string name, string id)
+    {
+        Name = name;
+        MemberId = id;
+    }
+    
+}
